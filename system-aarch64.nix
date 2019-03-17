1be6fbcfa226dc6e2b3139cff2edd7c5c2b0f040
Rehno Lindeque <errantkid@gmail.com>
2019-03-17 18:41:49 -0400

    Add filesystem for /boot mount

--- system-aarch64.nix
+++ system-aarch64.nix
@@ -6,7 +6,13 @@
   # nixpkgs.system = "aarch64-linux";
   deployment.ec2.ami = "ami-0d57f28ae76a680e3";
 
-  # boot.loader.grub.version = 2;
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/ESP";
+    autoResize = true;
+  };
+
+  boot.loader.grub.version = lib.mkForce 2;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.grub.device = lib.mkForce "nodev";
